7f25d74b4b41682d21acd4f4745b29b2151cd4fd
rafed01 <rafedriahi.rr@gmail.com>
2021-10-01 22:53:33 +0100

    nm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -display-the-alphabet a-z
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
