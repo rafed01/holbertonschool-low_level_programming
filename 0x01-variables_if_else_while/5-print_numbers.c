82ce50567fa8ebee45e623d6b8771245d53db803
rafed01 <rafedriahi.rr@gmail.com>
2021-10-01 22:58:54 +0100

    numbers with single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -numbers with single digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n <= 9 ; n++)
+printf("%d", n);
+putchar('\n');
+return (0);
+}
