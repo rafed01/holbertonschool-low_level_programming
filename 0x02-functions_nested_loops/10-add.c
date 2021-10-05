6890c178e7b97e8fbd05c4d10b403876c0a535b4
rafed01 <rafedriahi.rr@gmail.com>
2021-10-05 15:52:36 +0100

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers together
+ * @num1: num a to add
+ * @num2: num b to add
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ int _abs(int i);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
