369ea6f65c34b954f40b6e1b8abfa2aded00a390
rafed01 <rafedriahi.rr@gmail.com>
2021-10-05 12:35:27 +0100

    alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- alphabet
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
