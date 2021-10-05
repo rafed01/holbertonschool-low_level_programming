95e5ad137a42c44e535fb6e3cdac23c8cab9fec9
rafed01 <rafedriahi.rr@gmail.com>
2021-10-05 14:00:17 +0100

    lower upper

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _islower- check for lowercase
+ * _isalpha(int c)- checks for lowercase or uppercase
+ * @c: The character to check
  *
- * @c: an integer
- *
- * Return: 1 if c is lowercase 0 if c is not
+ * Return: 1 (Success)
  */
 int _isalpha(int c)
 {
-if (c <= 'z' && c >= 'a') || (c < 'z' && c > 'a')
-return (1);
-else
-return (0);
+	if ((c < 'z' && c > 'a') || (c < 'Z' && c > 'A'))
+		return (1);
+		else
+			return (0);
 }
