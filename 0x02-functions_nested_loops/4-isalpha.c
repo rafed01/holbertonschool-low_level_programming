362c8bb44f069169ef38cae5743a02f4d563afa1
rafed01 <rafedriahi.rr@gmail.com>
2021-10-05 13:57:36 +0100

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower- check for lowercase
+ *
+ * @c: an integer
+ *
+ * Return: 1 if c is lowercase 0 if c is not
+ */
+int _isalpha(int c)
+{
+if {
+(c <= 'z' && c >= 'a') || (c < 'z' && c > 'a')
+}
+return (1);
+else
+return (0);
+}
