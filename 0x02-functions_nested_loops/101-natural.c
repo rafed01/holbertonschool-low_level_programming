72a133606a53d723c006f975059e50e271049d7e
rafed01 <rafedriahi.rr@gmail.com>
2021-10-08 12:14:06 +0100

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+/**
+ * main- sum of numbers below 1024 that multiply 3 & 5
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    int x,s;
-    s = 0;
-    for (x=0 ; x <= 1023 ; x++)
-    {
-        if (x % 3 == 0)
-        s = s + x ;
-        else if (x % 5 == 0)
-        s = s + x ;
-        else if (x % 15 == 0)
-        s = s + x ;
-        else
-        s = s;
-    }
-    printf("%d\n", s);
-    return (0);
+int x,s;
+s = 0;
+for (x=0 ; x <= 1023 ; x++)
+{
+if (x % 3 == 0)
+s = s + x ;
+else if (x % 5 == 0)
+s = s + x ;
+else if (x % 15 == 0)
+s = s + x ;
+else
+s = s;
+}
+printf("%d\n", s);
+return (0);
 }
