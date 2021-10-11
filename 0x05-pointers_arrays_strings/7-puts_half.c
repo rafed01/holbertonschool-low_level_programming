bd8eae24a59b1d64e003990222943c72800334ec
rafed01 <rafedriahi.rr@gmail.com>
2021-10-11 11:38:12 +0100

    puts half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: pointer to string
+ */
+void puts_half(char *str)
+{
+int c;
+int len = _strlen(str);
+if (len % 2 == 0)
+{
+for (c = len / 2; c < len; c++)
+_putchar(str[c]);
+_putchar('\n');
+}
+else
+{
+for (c = (len / 2) + 1; c < len; c++)
+putchar(str[c]);
+putchar('\n');
+}
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+c++;
+return (c);
+}
