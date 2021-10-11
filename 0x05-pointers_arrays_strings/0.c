ef0bcaf8be939c5fbc296a583e2d747188edd972
rafed01 <rafedriahi.rr@gmail.com>
2021-10-11 11:31:43 +0100

    print every char

---
+++ 0x05-pointers_arrays_strings/0.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2 - prints a string, in reverse, followed by a new line
+ * @str: pointer to string
+ */
+void puts2(char *str)
+{
+int c;
+int len = _strlen(str);
+for (c = 0; c < len; c += 2)
+putchar(str[c]);
+putchar('\n');
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+c++;
+return (c);
+}
