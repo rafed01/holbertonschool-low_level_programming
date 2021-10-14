fdfe74fb3653ce060bfd7daa923e5657dd8be060
rafed01 <rafedriahi.rr@gmail.com>
2021-10-14 21:44:03 +0100

    cap all chars

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * @ch: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
