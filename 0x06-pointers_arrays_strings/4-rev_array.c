c1e1054409ea4ca773fd68d3f9562d819d63af09
rafed01 <rafedriahi.rr@gmail.com>
2021-10-14 12:46:06 +0100

    rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,11 +3,9 @@ void reverse_array(int *a, int n)
 {
 	int i, x, j = n;
 while (a[j])
-{
-    j--
-}
+    j--;
 
-	for (i = 0; i < n / 2; i++,)
+	for (i = 0; i < n ; i++)
 		{
 		x = a[i];
 		a[i] = a[j];
