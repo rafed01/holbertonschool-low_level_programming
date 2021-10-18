7fdcc482af9443d71a37166103c862d0a6c4780a
rafed01 <rafedriahi.rr@gmail.com>
2021-10-18 21:19:28 +0100

    memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: string to copy to 
+ * @src: string to copy from
+ * @n: how many chars to copy
+ * Return: the pointer to the char array
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
