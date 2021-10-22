d8dc247a343d72c99d48b31c2681c2fa85e569eb
rafed01 <rafedriahi.rr@gmail.com>
2021-10-22 10:50:40 +0100

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv : array of arguments
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
