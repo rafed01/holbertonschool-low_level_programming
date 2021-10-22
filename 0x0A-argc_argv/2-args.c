5917521d0a4910c33a4c552b1cfed48741672406
rafed01 <rafedriahi.rr@gmail.com>
2021-10-22 12:45:20 +0100

    print args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Prints all arguments it receives.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argc: number of args
+ * @argv: An array of args
  *
  * Return: Always 0.
  */
