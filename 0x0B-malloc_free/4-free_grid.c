0aadd2a4f2aa4c01632f92f6b1ad3fd76c587c7c
rafed01 <rafedriahi.rr@gmail.com>
2021-10-26 14:56:45 +0100

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,12 +2,13 @@
 /**
  * free_grid- free the grid in previous task
  * @grid : grid to be freed
- * @height : height of grid 
+ * @height : height of grid
  * Return: none
  */
 void free_grid(int **grid, int height)
 {
 	int x;
+
 	for (x = 0; x < height; x++)
 	{
 		free(grid[x]);
