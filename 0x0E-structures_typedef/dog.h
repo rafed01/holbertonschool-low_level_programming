d5112ff125fab5e76624010084b90251bde182a6
rafed01 <rafedriahi.rr@gmail.com>
2021-11-01 12:40:46 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,14 @@
 #define DOGH
 
 #include <stdio.h>
+/**
+ * struct dog - dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the owner
+ *
+ * Description: dog struct
+ */
 struct dog
 {
 	char *name;
